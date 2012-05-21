dcf9e1b7501e501ca8f6583505517f99e3b9670a
Saleem Shafi <saleemshafi@gmail.com>
2012-05-20 19:48:00 -0500

    change the app id because com.palm is not allowed

--- emulate.bat
+++ emulate.bat
@@ -1,4 +1,4 @@
 @echo off
 cmd /c palm-package .
-cmd /c palm-install com.palm.app.pushpop_1.0.0_all.ipk
-cmd /c palm-launch com.palm.app.pushpop
+cmd /c palm-install shafi.pushpop_1.0.0_all.ipk
+cmd /c palm-launch shafi.pushpop
